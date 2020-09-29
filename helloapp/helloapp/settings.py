156a2fdb446c90e1c06ce49148b2fdadecc32945
Jacob John <jacob.john@grofers.com>
2020-09-29 15:47:59 +0530

    updated settings.py

--- helloapp/helloapp/settings.py
+++ helloapp/helloapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
